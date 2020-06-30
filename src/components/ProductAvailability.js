1826da032e15e0270ec4bae0a3dabdfffb367120
Reid Ashwill <reidashwill@gmail.com>
2020-06-30 15:29:21 -0700

    add functions for each month and get them displaying dynamically

--- src/components/ProductAvailability.js
+++ src/components/ProductAvailability.js
@@ -1,28 +1,73 @@
 import React from 'react';
-import January from './ProductList';
+import {January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+} from './ProductList';
 
 class ProductAvailability extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      selectedMonth: "january"
+      selectedMonth: null
     };
   }
 
-  // handleClick = (month) => {
-  //   this.setState({selectedMonth: month});
-  // };
+  handleClick =(month) => {
+    this.setState({selectedMonth: month});
+  };
 
   render(){
     let currentlyVisibleState = null
     if (this.state.selectedMonth === "january") {
       currentlyVisibleState = <January />
+    }else if(this.state.selectedMonth ==='february'){
+      currentlyVisibleState = <February />
+    }else if(this.state.selectedMonth ==='march'){
+      currentlyVisibleState = <March />
+    }else if(this.state.selectedMonth ==='april'){
+      currentlyVisibleState = <April />
+    }else if(this.state.selectedMonth ==='may'){
+      currentlyVisibleState = <May />
+    }else if(this.state.selectedMonth ==='june'){
+      currentlyVisibleState = <June />
+    }else if(this.state.selectedMonth ==='july'){
+      currentlyVisibleState = <July />
+    }else if(this.state.selectedMonth ==='august'){
+      currentlyVisibleState = <August />
+    }else if(this.state.selectedMonth ==='september'){
+      currentlyVisibleState = <September />
+    }else if(this.state.selectedMonth ==='october'){
+      currentlyVisibleState = <October/>
+    }else if(this.state.selectedMonth ==='november'){
+      currentlyVisibleState = <November />
+    }else if(this.state.selectedMonth ==='december'){
+      currentlyVisibleState = <December />
     }
     return(
       <React.Fragment>
         {currentlyVisibleState}
-        {/* <button onClick={this.handleClick("january")}>January</button> */}
+        <button onClick={() => {this.handleClick("january")}}>January</button>
+        <button onClick={() => {this.handleClick("february")}}>February</button>
+        <button onClick={() => {this.handleClick("march")}}>March</button>
+        <button onClick={() => {this.handleClick("april")}}>April</button>
+        <button onClick={() => {this.handleClick("may")}}>May</button>
+        <button onClick={() => {this.handleClick("june")}}>June</button>
+        <button onClick={() => {this.handleClick("july")}}>July</button>
+        <button onClick={() => {this.handleClick("august")}}>August</button>
+        <button onClick={() => {this.handleClick("september")}}>September</button>
+        <button onClick={() => {this.handleClick("october")}}>October</button>
+        <button onClick={() => {this.handleClick("november")}}>November</button>
+        <button onClick={() => {this.handleClick("december")}}>December</button>
       </React.Fragment>
     );
   }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -278,7 +278,105 @@ function January(){
     </React.Fragment>
   )
 }
+function February(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[1].month}
+        selection={availableProduce[1].selection}/>
+    </React.Fragment>
+  )
+}
+function March(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[2].month}
+        selection={availableProduce[2].selection}/>
+    </React.Fragment>
+  )
+}
+function April(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[3].month}
+        selection={availableProduce[3].selection}/>
+    </React.Fragment>
+  )
+}
+function May(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[4].month}
+        selection={availableProduce[4].selection}/>
+    </React.Fragment>
+  )
+}
+function June(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[5].month}
+        selection={availableProduce[5].selection}/>
+    </React.Fragment>
+  )
+}
+function July(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[6].month}
+        selection={availableProduce[6].selection}/>
+    </React.Fragment>
+  )
+}
+function August(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[7].month}
+        selection={availableProduce[7].selection}/>
+    </React.Fragment>
+  )
+}
+function September(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[8].month}
+        selection={availableProduce[8].selection}/>
+    </React.Fragment>
+  )
+}
+function October(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[9].month}
+        selection={availableProduce[9].selection}/>
+    </React.Fragment>
+  )
+}
+function November(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[10].month}
+        selection={availableProduce[10].selection}/>
+    </React.Fragment>
+  )
+}
+function December(){
+  return(
+    <React.Fragment>  
+        <Month month={availableProduce[11].month}
+        selection={availableProduce[11].selection}/>
+    </React.Fragment>
+  )
+}
 
-export default January
-
-// availableProduce[11].selection[4]
+export {January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
